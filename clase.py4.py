11ea35a10c7c86ed14d9bb4fe3abc04c36813f91
benjaminalegria <be.alegriap@duocuc.cl>
2025-04-24 15:43:16 -0400

    Add files via upload

---
+++ clase.py4.py
@@ -0,0 +1,29 @@
+"""
+
+nombre = input("dime tu nombre")
+
+def saludo(nombre):
+    print(f"hola, {nombre}, adios...")
+
+
+saludo(nombre) 
+
+
+
+
+
+def suma(a,b):
+    return a + b
+resultado = suma(5,6)
+print("el resultrado: " , resultado)
+
+
+debemos realiar un programa indice un rango insertando un valor para generar otro aleatorio dentro de ese mismo rango continuando el programa 
+
+"""
+
+
+import random
+numero_aleatorio = random.randint(1, 100)  # Genera un número entre 1 y 100
+print(numero_aleatorio)
+
